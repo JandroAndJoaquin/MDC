42f800ffae5c51d50db73174ff21c2cee8e80d88
rojasjandro89 <rojasjandro89@gmail.com>
2016-12-31 13:20:08 -0500

    Test

--- app/src/main/java/com/example/android/mdc/activities/LoginActivity.java
+++ app/src/main/java/com/example/android/mdc/activities/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.mdc.activities;
 
+import android.graphics.Typeface;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.android.mdc.R;
 
@@ -9,16 +12,20 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView title1, title2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        title1 = (TextView) findViewById(R.id.login_title_1);
+        title2 = (TextView) findViewById(R.id.login_title_2);
 
-        CalligraphyConfig.initDefault(
-                new CalligraphyConfig.Builder()
-                        .setDefaultFontPath("Roboto-Thin.ttf")
-                        .setFontAttrId(R.attr.fontPath)
-                        .build()
-        );
+        Typeface robotoLight = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
+        Typeface robotoRegular = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+        Typeface robotoBold = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Bold.ttf");
+
+        title1.setTypeface(robotoLight);
+        title2.setTypeface(robotoBold);
     }
 }
